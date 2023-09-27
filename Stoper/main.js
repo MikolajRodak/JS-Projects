2fc01fb3e20bf260023583d0542d5b3b38c5e0a5
Mikołaj <mrodak03@gmail.com>
2023-09-27 15:18:43 +0200

    finished stoper

--- Stoper/main.js
+++ Stoper/main.js
@@ -6,9 +6,17 @@ const historyBtn = document.querySelector('.history')
 const stopwatch = document.querySelector('.stopwatch')
 const time = document.querySelector('.time')
 const timeList = document.querySelector('.time-list')
-const infoBtn = document.querySelector('.info')
+const infoBtn = document.querySelector('.question')
 const modalShadow = document.querySelector('.modal-shadow')
 const closeBtn = document.querySelector('.close')
+
+const colorsMenu = document.querySelector('.colors')
+const blueColorBtn = document.querySelector('.one')
+const redColorBtn = document.querySelector('.two')
+const greenColorBtn = document.querySelector('.three')
+const brushBtn = document.querySelector('.brush')
+
+let root = document.documentElement
 
 let countTime
 let minutes = 0
@@ -30,7 +38,7 @@ const handleSatrt = () => {
 			minutes++
 			stopwatch.textContent = `${minutes}:${seconds}`
 		}
-	}, 100)
+	}, 1000)
 }
 
 const handlePause = () => {
@@ -92,3 +100,22 @@ historyBtn.addEventListener('click', showHistory)
 infoBtn.addEventListener('click', handleModal)
 closeBtn.addEventListener('click', handleModal)
 window.addEventListener('click', e => (e.target === modalShadow ? handleModal() : false))
+
+brushBtn.addEventListener('click', () => {
+	colorsMenu.classList.toggle('showColors')
+})
+
+redColorBtn.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(250, 20, 6)')
+	root.style.setProperty('--hover-color', 'rgb(200, 8, 0)')
+})
+
+blueColorBtn.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(29, 96, 179)')
+	root.style.setProperty('--hover-color', 'rgb(21, 67, 124)')
+})
+
+greenColorBtn.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(3, 252, 23)')
+	root.style.setProperty('--hover-color', 'rgb(35, 155, 45)')
+})
